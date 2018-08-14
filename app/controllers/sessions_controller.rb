b56c540983c1e790d2b72f30b952b3ceaee1cc5f
Vanessa Toro <vanetoro84@gmail.com>
2018-08-14 17:59:10 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-  resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
+  @resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
     req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
     req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
     req.params['grant_type'] = 'authorization_code'
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     req.params['code'] = params[:code]
   end
 
-  body = JSON.parse(resp.body)
+  body = JSON.parse(@resp.body)
   session[:token] = body["access_token"]
   redirect_to root_path
 end
